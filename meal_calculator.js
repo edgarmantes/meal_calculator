6e51c7a54d5d4bc8afd3251933b81fbcadc7e72c
Edgar Mantes <edgar.mantes11@gmail.com>
2016-11-26 13:36:42 -0600

    completed methods for Diner object and Bills object

--- meal_calculator.js
+++ meal_calculator.js
@@ -2,6 +2,7 @@ var Diner = function(name){
 	this.name = name;
 	this.total = null;
 	this.tip = null;
+	this.tax = null;
 	this.dishes = []
 };
 
@@ -9,52 +10,92 @@ Diner.prototype.allDishes = function(item, price){
 	var meal = {
 		dishName : item,
 		price : price
-	}
+	};
 	this.dishes.push(meal);
 };
 
 Diner.prototype.addPrice = function(){
-
+	var price = 0;
+	for (var i of this.dishes){
+		price += i.price;
+	}
+	this.total = price;
 };
 
-Diner.prototype.tax = function(){
-
+Diner.prototype.taxes = function(){
+	var mealTax = this.total * 0.11
+	this.tax = mealTax;
 };
 
-Diner.prototype.tip = function(){
-
+Diner.prototype.mealTip = function(){
+	var tiping = (this.total + this.tax) * 0.15;
+	this.tip = tiping.toFixed(2);
 };
 
 
 
 
 
-var Bill = function(diners,){
-	this.diners = diners;
+var Bill = function(){
+	this.diners = [];
 	this.totalBill = null;
 	this.totalTaxes = null;
 	this.totalTips = null;
 };
 
 Bill.prototype.billTotal = function(){
-
+	for ( var i of this.diners){
+		this.totalBill += i.total
+	};
 };
 
 Bill.prototype.taxesTotal = function(){
-
+	for ( var i of this.diners){
+		this.totalTaxes += i.tax;
+	};
+	//console.log('taxes are ' + this.totalTaxes)
 };
 
 Bill.prototype.tipsTotal = function(){
-
+	for ( var i of this.diners){
+		this.totalTips += i.tip;
+	};
+	//console.log('tips total is ' + this.totalTips)
 };
 
 var data = {
 
 };
 
-
+var bills = new Bill();
 
 
 var Kristen = new Diner('Kristen');
 Kristen.allDishes('Lobster Sandwich', 15);
 Kristen.allDishes('Spicy Beef Salad', 5);
+bills.diners.push(Kristen);
+Kristen.addPrice();
+Kristen.taxes();
+Kristen.mealTip();
+
+var Edgar = new Diner('Edgar');
+Edgar.allDishes('Lobster', 25);
+Edgar.allDishes('Tachos', 10);
+bills.diners.push(Edgar);
+Edgar.addPrice();
+Edgar.taxes();
+Edgar.mealTip();
+
+var Jessica = new Diner('Jessica');
+Jessica.allDishes('Steak Fillet', 20);
+Jessica.allDishes('Chili Cheese Fries', 8);
+bills.diners.push(Jessica);
+Jessica.addPrice();
+Jessica.taxes();
+Jessica.mealTip();
+
+bills.billTotal();
+bills.taxesTotal();
+bills.tipsTotal();
+
+console.log(bills.diners);
